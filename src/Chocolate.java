35cd5a42753aeb7ac0a53dcba7def0ff3f9c200b
Michael <mblakey2@ggc.edu>
2023-02-01 01:34:17 -0500

    add Chocolate.java

--- src/Chocolate.java
+++ src/Chocolate.java
@@ -11,4 +11,19 @@
 
 
 public class Chocolate {
+    public static void main(String[]args){
+        int BagsOfChocolate = 9;
+        int Chocolate_in_Each_Bag = 2;
+        int Bars_to_fill_bags = 3;
+        int Chocolate_bars_total = BagsOfChocolate * Chocolate_in_Each_Bag;
+        int Number_of_bags_for_three_bars_each = Chocolate_bars_total / Bars_to_fill_bags;
+        System.out.println("Bags of Chocolate:" + BagsOfChocolate);
+        System.out.println("# of bars in bag:" + Chocolate_in_Each_Bag);
+        System.out.println("# of bars to fill bag:" + Bars_to_fill_bags);
+        System.out.println("Chocolate bars in total:" + Chocolate_bars_total);
+        System.out.println("Bags needed for three bars each:" + Number_of_bags_for_three_bars_each);
+
+
+
+    }
 }
