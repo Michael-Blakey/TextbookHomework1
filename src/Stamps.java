4ecde448ddf4d71610092ad6ea3ee42603b37624
Michael <mblakey2@ggc.edu>
2023-02-01 02:01:41 -0500

    add Stamps.java

---
+++ src/Stamps.java
@@ -0,0 +1,12 @@
+public class Stamps {
+    public static void main(String[]args){
+        int Susan_Stamps = 8;
+        int Jean_Stamps = 40;
+        int Stamps_Jean_has_more_then_Susan = Jean_Stamps - Susan_Stamps;
+        System.out.print("Susan's stamps:" + Susan_Stamps);
+        System.out.print("\njean's stamps:" + Jean_Stamps);
+        System.out.print("\nStamps that Jean has more than Susan:" + Stamps_Jean_has_more_then_Susan);
+
+
+    }
+}
