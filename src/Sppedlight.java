dd28926f0a9e18605ddf9704a9ed4f69e403e0da
Michael <mblakey2@ggc.edu>
2023-01-31 22:50:28 -0500

    added commented line

---
+++ src/Sppedlight.java
@@ -0,0 +1,18 @@
+/**
+ * Class Name: Speedlight
+ * Author: Michael Blakey
+ * Version: 1.0
+ * Date: 26 January 2023
+ */
+
+
+
+
+
+
+
+public class Sppedlight {
+    public static void main(String[]args){
+
+    }
+}
