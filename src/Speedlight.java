c4cee57f63e2b2e5dfcb5347794735ad62a2cbcd
Michael <mblakey2@ggc.edu>
2023-02-01 01:00:21 -0500

    add Speedlight.java

---
+++ src/Speedlight.java
@@ -0,0 +1,26 @@
+/**
+ * Class Name: Speedlight
+ * Author: Michael Blakey
+ * Version: 1.0
+ * Date: 26 January 2023
+ */
+
+
+
+
+
+
+
+public class Speedlight {
+    public static void main(String[]args){
+        int SPEED_OF_SOUND = 340;
+        int TimeElapsed = 5;
+        int distance = SPEED_OF_SOUND * TimeElapsed;
+        System.out.println("speed of sound:" + SPEED_OF_SOUND);
+        System.out.println("Time Elapse:" + TimeElapsed);
+        System.out.println("Distance:" + distance);
+
+
+
+    }
+}
