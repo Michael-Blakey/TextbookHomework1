3b92b074207c98423db2e2d88334dc4b27e8406d
Michael <mblakey2@ggc.edu>
2023-01-31 23:01:30 -0500

    add Chocolate.java

---
+++ src/Chocolate.java
@@ -0,0 +1,14 @@
+/**
+ * Class Name: Speedlight
+ * Author: Michael Blakey
+ * Version: 1.0
+ * Course: ITEC 2140 Section 01 Spring 2023
+ * Date: 26 January 2023
+ */
+
+
+
+
+
+public class Chocolate {
+}
