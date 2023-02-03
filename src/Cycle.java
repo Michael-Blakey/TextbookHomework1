bb0261ece5a2d93db5d98fdd86c2e9411244ef15
Michael <mblakey2@ggc.edu>
2023-02-02 22:33:06 -0500

    add Cycle.java

--- src/Cycle.java
+++ src/Cycle.java
@@ -1,3 +1,16 @@
+/**
+ * @class: Cycle
+ * @author: Michael Blakey
+ * @course: ITEC2148 - 05, Spring 2023
+ * @version: 1.0
+ * @date: February 2, 2023
+ *
+ *
+ *
+ */
+
+
+
 public class Cycle {
     public static void main (String[]args){
         int number_of_bicycles = 10;
@@ -7,8 +20,9 @@ public class Cycle {
         int bicycles_in_shop = number_of_bicycles * wheels_on_bicycles;
         System.out.println("Bikes in the shop:" + bicycles_in_shop);
         System.out.println("47 - 20:" + (number_of_wheels_total - bicycles_in_shop));
+        //27 is the number given after subtracting 47 to 20.
         System.out.println("tricycles in the shop:" + 27 / wheels_on_tricycles);
-
+// 27 is how many wheels are left in the shop after bike wheels. So i divided the wheels of trics to 27 ro get my total.
 
 
     }
