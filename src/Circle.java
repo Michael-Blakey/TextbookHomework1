1917402782c7aecd8c1ba768802b2d2995e15ae7
Michael <mblakey2@ggc.edu>
2023-02-02 22:30:21 -0500

    add Circle.java

--- src/Circle.java
+++ src/Circle.java
@@ -1,6 +1,19 @@
+/**
+ * @class: Circle
+ * @author: Michael Blakey
+ * @course: ITEC2148 - 05, Spring 2023
+ * @version: 1.0
+ * @date: February 2, 2023
+ *
+ *
+ */
+
+
+
+
 public class Circle {
     public static void main(String[]args){
-        double radius = 9.5;
+        double radius = 9.5; //double is needed due to the number being 9.5.
         double area = radius * radius * Math.PI;
         double perimeter = 2 * radius * Math.PI;
         System.out.println("Area of the circle is = " + area);
