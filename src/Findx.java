70054bc85e5ac4f2b709ddf0347f8efe28aeb210
Michael <mblakey2@ggc.edu>
2023-02-02 21:03:54 -0500

    add Findx.java

---
+++ src/Findx.java
@@ -0,0 +1,16 @@
+public class Findx {
+    public static void main(String[]args){
+        int a = 5;
+        int b = 19;
+        int x;
+        int c = 47;
+        int sum = 194;
+        int added_numbers_we_have = a + b + c;
+        x = a + b + c - sum;
+        System.out.println("X=:" + (sum - added_numbers_we_have ));
+
+
+
+
+    }
+}
