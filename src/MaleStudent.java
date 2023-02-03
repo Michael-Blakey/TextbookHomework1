0ef94e855c59af1bb06c36143ec59a16bbe9326c
Michael <mblakey2@ggc.edu>
2023-02-02 21:10:53 -0500

    add MaleStudent.java

---
+++ src/MaleStudent.java
@@ -0,0 +1,8 @@
+public class MaleStudent {
+    public static void main(String [] args){
+        int total_students = 389;
+        int female_students = 175;
+        int male_students = total_students - female_students;
+        System.out.println("Male students in the school:" + male_students);
+    }
+}
